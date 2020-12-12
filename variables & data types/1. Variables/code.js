3c73f3127cce0091d5117f65cb22e18ab4ab0a4f
Jair Alayon <jair.atl.95@gmail.com>
2020-12-11 22:28:59 -0600

    Initial commit

---
+++ variables & data types/1. Variables/code.js
@@ -0,0 +1,15 @@
+//declare a variable
+var score;
+console.log(score);
+
+//declare a variable and initialize
+var livesLeft = 3;
+console.log(livesLeft);
+
+//re-asign a value to our variable
+livesLeft = 5
+console.log(livesLeft);
+
+//constants
+const firstname = 'Jair';
+console.log(firstname);
